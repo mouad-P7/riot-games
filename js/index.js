10308d235b34fcd585d878bed88469904ffb64eb
mouad-p7 <mouadananouch7@gmail.com>
2022-09-08 13:03:18 +0100

    I added links to all my <a> tags and i made dropdowns in my nav-bar.

--- js/index.js
+++ js/index.js
@@ -4,16 +4,24 @@ $(document).ready(function(){
         let gap = 100;
         if($(window).scrollTop() > gap){
             $('.home-header').addClass('scroll-down');
-        } else{
+        }else{
             $('.home-header').removeClass('scroll-down');
         }
     });
 
     $('.right-menu-tablet-container').click(function(){
         $('.mobile-nav-bar').addClass('visible-nav-bar');
+        $('.home-body').css({'opacity':'0.7'});
     });
     
     $('.close-menu-tablet-container').click(function(){
         $('.mobile-nav-bar').removeClass('visible-nav-bar');
+        $('.home-body').css({'opacity':'1'});
+    });
+
+    $('.dropdown-container').hover(function(){
+        $(this).find('.dropdown').css({'opacity':'1', 'pointer-events':'auto'});
+    }, function(){
+        $(this).find('.dropdown').css({'opacity':'0', 'pointer-events':'none'});
     });
 });
