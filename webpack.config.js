433618446a9cae7ffd93d4b054542fa79530c47f
mouad-p7 <mouadananouch7@gmail.com>
2023-01-11 20:00:45 +0100

    I implemented cache busting to main.js, and i added html-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require("path");
+const htmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
     devtool: "eval",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main-[contentHash].js",
         path: path.resolve(__dirname,"dist")
     },
+    plugins: [
+        new htmlPlugin({template: "template.html"})
+    ],
     module: {
         rules: [
             {test: /\.css$/, use: ["style-loader","css-loader"]}
