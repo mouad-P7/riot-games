c60575df17a116fba1a61cfc8b85659cb67cae02
mouad-p7 <mouadananouch7@gmail.com>
2023-01-11 21:40:10 +0100

    I instaled webpack-merge package, and I set up development and production configuration mode for webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
-const path = require("path");
 const htmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "eval",
     entry: "./src/index.js",
-    output: {
-        filename: "main-[contentHash].js",
-        path: path.resolve(__dirname,"dist")
-    },
     plugins: [
         new htmlPlugin({template: "template.html"})
     ],
